e2e5c665e8ed531a11e3e935525a50173dd54249
Jamiras <jamirasmt@gmail.com>
2026-09-10 08:43:35 -0600

    prevent scrollwheel while IPE is open

--- src/ui/win32/bindings/GridBinding.cpp
+++ src/ui/win32/bindings/GridBinding.cpp
@@ -852,6 +852,16 @@ void GridBinding::SetHWND(DialogBase& pDialog, HWND hControl)
 
         if (m_vmItems)
             UpdateAllItems();
+
+        // if an editable column exists, hook the wndproc so we can prevent scrolling while editing.
+        for (const auto& pColumn : m_vColumns)
+        {
+            if (!pColumn->IsReadOnly())
+            {
+                SubclassWndProc();
+                break;
+            }
+        }
     }
 
 #ifdef UNICODE
@@ -910,6 +920,12 @@ INT_PTR CALLBACK GridBinding::WndProc(HWND hControl, UINT uMsg, WPARAM wParam, L
                     }
                 }
             }
+            break;
+
+        case WM_MOUSEWHEEL:
+            // prevent scrolling if the IPE is open
+            if (m_hInPlaceEditor)
+                return 0;
             break;
     }
 
